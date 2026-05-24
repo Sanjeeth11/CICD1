df3058af416cf7a08e7bbb45dbf86be16fa74e99
Sanjeeth11 <pisupatysaisanjeethraj@gmail.com>
2026-05-25 00:24:41 +0530

    firstcommit

--- src/main/java/com/organization/employees/serviceImpl/EmployeeServiceImpl.java
+++ src/main/java/com/organization/employees/serviceImpl/EmployeeServiceImpl.java
@@ -3,12 +3,13 @@ package com.organization.employees.serviceImpl;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.organization.employees.entity.Employee;
 import com.organization.employees.repo.EmployeeRepo;
 import com.organization.employees.service.EmployeeService;
 
-
+@Service
 public class EmployeeServiceImpl implements EmployeeService {
 
 	@Autowired
