405edc3f096b291e8a51fd916661f5124b38a7d5
Sanjeeth11 <pisupatysaisanjeethraj@gmail.com>
2026-05-26 22:25:01 +0530

    Update EmployeesApplication.java

--- src/main/java/com/organization/employees/EmployeesApplication.java
+++ src/main/java/com/organization/employees/EmployeesApplication.java
@@ -2,12 +2,18 @@ package com.organization.employees;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EmployeesApplication {
+public class EmployeesApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EmployeesApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(EmployeesApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeesApplication.class, args);
+    }
 }
